183371567c5db49df505f5b76c33d5375e27c18c
sisayj <87474568+sisayj@users.noreply.github.com>
2024-03-10 15:12:38 -0400

    Delete payment.py

--- payment.py
+++
@@ -1,7 +0,0 @@
-# payment.py
-class Payment:
-    def __init__(self, ride_id, amount, payment_method, status="Pending"):
-        self.ride_id = ride_id
-        self.amount = amount
-        self.payment_method = payment_method
-        self.status = status
